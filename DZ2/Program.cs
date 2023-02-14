8c423fbffc4a4316964dc030fa73c6b58eff5ac0
Muhammad <type.92@mail.ru>
2023-02-15 00:38:26 +0300

    END

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -9,5 +9,5 @@
 Console.WriteLine("Введите трехзначное число");
 string str = Console.ReadLine();
 if (str.All(Char.IsDigit) && str.Length >= 3) Console.WriteLine($"Третья цифра {str[2]}");
-else if (str.All(Char.IsDigit))Console.WriteLine("Нет третьей цифры");
+else if (str.All(Char.IsDigit)) Console.WriteLine("Нет третьей цифры");
 else Console.WriteLine("Ошибка!!! Введите только цифры!");
